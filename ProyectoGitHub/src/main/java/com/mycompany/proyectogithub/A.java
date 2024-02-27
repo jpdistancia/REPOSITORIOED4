0a5e2004a112ea9ef7c3425bf26722d3b6b7afd7
Isabel-Puertas9 <i.puertas.planes@gmail.com>
2024-02-27 18:51:19 +0100

    Subir los archivos a GitHub

---
+++ ProyectoGitHub/src/main/java/com/mycompany/proyectogithub/A.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectogithub;
+
+/**
+ *
+ * @author Profesor
+ */
+public class A 
+{
+    public int atr1;
+
+    public A(int atr1) 
+    {
+        this.atr1 = atr1;
+    }
+
+    public int getAtr1() 
+    {
+        return atr1;
+    }
+
+    public void setAtr1(int atr1) 
+    {
+        this.atr1 = atr1;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ ProyectoGitHub/src/main/java/com/mycompany/proyectogithub/ProyectoGitHub.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proyectogithub;
+
+/**
+ *
+ * @author Profesor
+ */
+public class ProyectoGitHub 
+{
+
+    public static void main(String[] args) 
+    {
+        A objA = new A(5);
+        
+        System.out.println(objA.getAtr1());
+    }
+}
